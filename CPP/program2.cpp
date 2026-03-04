d14a43fb44b33244d5db122b924f68d5181469ae
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-03-04 22:35:37 +0530

    Add files via upload

---
+++ CPP/program2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=0;
+    int j=0;
+    int ans=0;
+
+    cout<<"Enter the first no:\n";
+    cin>>i;
+
+    cout<<"Enter the second no:\n";
+    cin>>j;
+
+    ans = i + j;
+    
+    cout<<"Addition is:"<<ans<<endl;
+
+    return 0;
+}
