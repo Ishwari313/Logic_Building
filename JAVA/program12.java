e23587a2767cc18ce3e8802412ecb63626529705
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-03-14 22:59:13 +0530

    Add files via upload

---
+++ JAVA/program12.java
@@ -0,0 +1,16 @@
+class Marvellous
+{
+    public void Display()
+    {
+        System.out.println("Jay Ganesh");
+    }
+}
+class program12
+{
+    public static void main(String Arr[])
+    {
+        Marvellous mobj= new Marvellous(); 
+        mobj.Display();
+    
+    }                                                                                
+}
